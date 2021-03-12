32f3bcebc6563aefc74c9014255f9c04d0332689
Suryadip Bandyopadhyay <sband2000@gmail.com>
2021-03-12 17:37:19 -0600

    added demo pages

---
+++ demo/share/footer.php
@@ -0,0 +1,128 @@
+<footer class="footer">
+            <div class="container">
+                <div class="footer-top">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <a href="demo14.html" class="logo-footer">
+                                <img src="images/logo-footer.png" alt="logo-footer" width="163" height="39" />
+                            </a>
+                            <!-- End FooterLogo -->
+                        </div>
+                        <div class="col-lg-9">
+                            <div class="widget widget-newsletter form-wrapper form-wrapper-inline">
+                                <div class="newsletter-info ml-lg-4 mx-auto">
+                                    <h4 class="widget-title">Subscribe to our Newsletter</h4>
+                                    <p>Get all the latest information on Events, Sales and Offers.</p>
+                                </div>
+                                <form action="#" class="input-wrapper input-wrapper-inline">
+                                    <input type="email" class="form-control" name="email" id="email"
+                                        placeholder="Email address here..." required />
+                                    <button class="btn btn-primary btn-md ml-2" type="submit">subscribe<i
+                                            class="d-icon-arrow-right"></i></button>
+                                </form>
+                            </div>
+                            <!-- End Newsletter -->
+                        </div>
+                    </div>
+                </div>
+                <!-- End FooterTop -->
+                <div class="footer-middle">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="widget">
+                                <h4 class="widget-title">Contact Info</h4>
+                                <ul class="widget-body">
+                                    <li>
+                                        <label>Phone:</label>
+                                        <a href="#">Toll Free (123) 456-7890</a>
+                                    </li>
+                                    <li>
+                                        <label>Email:</label>
+                                        <a href="#">example@gmail.com</a>
+                                    </li>
+                                    <li>
+                                        <label>Address:</label>
+                                        <a href="#">123 Street Name, City, England</a>
+                                    </li>
+                                    <li>
+                                        <label>WORKING DAYS/HOURS</label>
+                                    </li>
+                                    <li>
+                                        <a href="#">Mon - Sun / 9:00 AM - 8:00 PM</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- End Widget -->
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="widget ml-lg-4">
+                                <h4 class="widget-title">My Account</h4>
+                                <ul class="widget-body">
+                                    <li>
+                                        <a href="javascript:void(0)">About Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">Order History</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">Returns</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">Custom Service</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">Terms &amp; Condition</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- End Widget -->
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="widget ml-lg-4">
+                                <h4 class="widget-title">Contact Info</h4>
+                                <ul class="widget-body">
+                                    <li>
+                                        <a href="javascript:void(0)">Payment Methods</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">Money-back Guarantee!</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">Returns</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">Shipping</a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">Terms and Conditions</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- End Widget -->
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <!-- End Instagram -->
+                        </div>
+                    </div>
+                </div>
+                <!-- End FooterMiddle -->
+                <div class="footer-bottom">
+                    <div class="footer-left">
+                        <figure class="payment">
+                            <!-- <img src="images/payment.png" alt="payment" width="159" height="29" /> -->
+                        </figure>
+                    </div>
+                    <div class="footer-center">
+                        <p class="copyright"> &copy; 2020. All Rights Reserved</p>
+                    </div>
+                    <div class="footer-right">
+                        <div class="social-links">
+                            <a href="javascript:void(0)" class="social-link social-facebook fab fa-facebook-f"></a>
+                            <a href="javascript:void(0)" class="social-link social-twitter fab fa-twitter"></a>
+                            <a href="javascript:void(0)" class="social-link social-linkedin fab fa-linkedin-in"></a>
+                        </div>
+                    </div>
+                </div>
+                <!-- End FooterBottom -->
+            </div>
+        </footer>
---
+++ demo/share/header.php
@@ -0,0 +1,170 @@
+
+<script>
+  function initFreshChat() {
+    window.fcWidget.init({
+      token: "a445c7cb-15d7-4586-87be-3e9b4b68ba1a",
+      host: "https://wchat.in.freshchat.com"
+    });
+  }
+  function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.in.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
+</script>
+
+
+<header class="header">
+            <div class="header-top">
+                <div class="container">
+                    <div class="free-shipping">
+                    FREE SHIPPING on Orders of $199+
+                    </div>
+                    <div class="header-left d-none">
+                        <p class="welcome-msg">Welcome to shopper message or remove it!</p>
+                    </div>
+                    <div class="header-right  d-none">
+                        <div class="dropdown currency-dropdown">
+                            <a href="javascript:void(0)">USD</a>
+                            <ul class="dropdown-box">
+                                <li><a href="javascript:void(0)">USD</a></li>
+                                <li><a href="javascript:void(0)">EUR</a></li>
+                            </ul>
+                        </div>
+                        <!-- End DropDown Menu -->
+                        <div class="dropdown language-dropdown">
+                            <a href="#language"><img src="images/flags/en.png" alt="USA Flag"
+                                    class="dropdown-image" />ENG</a>
+                            <ul class="dropdown-box">
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="images/flags/en.png" alt="USA Flag" class="dropdown-image" />ENG
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="images/flags/fr.png" alt="France Flag" class="dropdown-image" />FRH
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- End DropDown Menu -->
+                        <div class="dropdown dropdown-expanded d-lg-show">
+                            <a href="#dropdown">Links</a>
+                            <ul class="dropdown-box">
+                                <li><a href="javascript:void(0)">About</a></li>
+                                <li><a href="javascript:void(0)">Blog</a></li>
+                            </ul>
+                        </div>
+                        <!-- End DropDownExpanded Menu -->
+                    </div>
+                </div>
+            </div>
+            <!-- End HeaderTop -->
+            <div class="header-middle has-center">
+                <div class="container">
+                    <div class="header-left">
+                        <a href="#" class="mobile-menu-toggle">
+                            <i class="d-icon-bars2"></i>
+                        </a>
+                        <!-- Mobile Menu Toggle -->
+                        <div class="header-center">
+                            <a href="javascript:void(0)" class="logo mr-0">
+                                <img src="https://via.placeholder.com/160x40" alt="logo" width="163" height="39" />
+                            </a>
+                            <!-- End Logo -->
+                        </div>
+                    </div>
+                    
+                    <div class="header-search hs-simple">
+                        <form action="#" class="input-wrapper">
+                            <input type="text" class="form-control" name="search" autocomplete="off"
+                                placeholder="Search..." required />
+                            <button class="btn btn-search" type="submit">
+                                <i class="d-icon-search"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <div class="header-right-phone-number">
+                        <a href="tel:+1234560789">123.456.0789</a><br> 
+                        Talk to one of our Experts 
+                    </div>
+                    <!-- End Header Search -->
+                    <div class="header-right">
+                        <a class="login" href="javascript:void(0)">
+                            <i class="d-icon-user"></i>
+                            <span>Sign in</span>
+                        </a>
+                        <!-- End Login -->
+                        <span class="divider"></span>
+                        <a class="login" href="javascript:void(0)">
+                            <span>Help</span>
+                        </a>
+                        <!-- End Help -->
+                        <span class="divider"></span>
+                        <div class="dropdown cart-dropdown">
+                            <a href="#" class="cart-toggle">
+                                <span class="cart-label">
+                                    <span class="cart-name">My Cart</span>
+                                    <span class="cart-price">$42.00</span>
+                                </span>
+                                <i class="minicart-icon">
+                                    <span class="cart-count">2</span>
+                                </i>
+                            </a>
+                            <!-- End Cart Toggle -->
+                            
+                        </div>
+                        <div class="header-search hs-toggle mobile-search">
+                            <a href="#" class="search-toggle">
+                                <i class="d-icon-search"></i>
+                            </a>
+                            <form action="#" class="input-wrapper">
+                                <input type="text" class="form-control" name="search" autocomplete="off"
+                                    placeholder="Search your keyword..." required />
+                                <button class="btn btn-search" type="submit">
+                                    <i class="d-icon-search"></i>
+                                </button>
+                            </form>
+                        </div>
+                        <!-- End of Header Search -->
+                    </div>
+                </div>
+
+            </div>
+            <!-- End Header Middle -->
+            <div class="header-bottom sticky-header fix-top sticky-content has-dropdown">
+                <div class="container">
+                    <div class="inner-wrap">
+                        <div class="header-left">
+                            <nav class="main-nav">
+                                <ul class="menu">
+                                    <li class="active">
+                                        <a href="javascript:void(0)"> Custom Cellars </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)"> Cooling & Storage </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)"> Furniture </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)"> Glassware </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)"> Accessories </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)"> Housewares </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">  Wine Gifts  </a>
+                                    </li>
+                                    <li>
+                                        <a href="javascript:void(0)">  Learn  </a>
+                                    </li>
+                                    
+                                    
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
